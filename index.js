d124b2b4e8482cdb4e1b87fc86282de000ccfea7
Sindre Sorhus <sindresorhus@gmail.com>
2014-01-19 18:46:42 +0100

    switch to through2

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 'use strict';
 var gutil = require('gulp-util');
-var map = require('map-stream');
+var through = require('through2');
 var stripJsonComments = require('strip-json-comments');
 
-module.exports = function (bowerConfig, options) {
-	return map(function (file, cb) {
+module.exports = function () {
+	return through.obj(function (file, enc, cb) {
 		if (file.isNull()) {
-			return cb(null, file);
+			this.push(file);
+			return cb();
 		}
 
-		file.contents = new Buffer(stripJsonComments(file.contents.toString()));
-		cb(null, file);
+		if (file.isStream()) {
+			this.emit('error', new gutil.PluginError('gulp-strip-json-comments', 'Streaming not supported'));
+			return cb();
+		}
+
+		try {
+			file.contents = new Buffer(stripJsonComments(file.contents.toString()));
+		} catch (err) {
+			this.emit('error', new gutil.PluginError('gulp-strip-json-comments', err));
+		}
+
+		this.push(file);
+		cb();
 	});
 };
