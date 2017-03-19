5e78b62ace6964448753764d20f286bfa608718c
Michel Milano <mmilano+git@gmail.com>
2017-03-18 17:50:29 -0700

    changes to the test to add new whitespace test

--- test.js
+++ test.js
@@ -15,3 +15,18 @@ it('should strip JSON comments', function (cb) {
 		contents: new Buffer('//comment\n{"a":"b"}')
 	}));
 });
+
+it('should strip JSON comments and whitespace with options', function (cb) {
+	var options = {whitespace: false};
+	var stream = stripJsonComments(options);
+
+	stream.on('data', function (file) {
+		assert.equal(file.contents.toString(), '\n{"a":"b"}');
+		cb();
+	});
+
+	stream.write(new gutil.File({
+		contents: new Buffer('//comment   \n{"a":"b"}')
+	}));
+});
+
